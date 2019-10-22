ca89a3e5eec077bf1247a603ab047e391a449e55
Vyacheslav Lapin <vyacheslav@y-tree.com>
2019-10-22 17:56:30 +0500

    Initial commit

---
+++ src/main/java/ru/vlapin/experiments/refactoringdemo/RefactoringDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.vlapin.experiments.refactoringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactoringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vlapin/experiments/refactoringdemo/RefactoringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vlapin.experiments.refactoringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactoringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
